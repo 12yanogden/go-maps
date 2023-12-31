cf42bdb15e102085501b52e30d850b496239d1d7
Ryan Ogden <12yanogden@gmail.com>
2023-12-31 10:10:19 -0500

    paste func()s from strmaps package

--- maps.go
+++ maps.go
@@ -1,5 +1,50 @@
 package maps
 
+import (
+	"cmp"
+	"slices"
+)
+
+// Return keys of the map given
+func Keys[K comparable, V any](m map[K]V) []K {
+	var keys []K
+
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	return keys
+}
+
+// Return a sorted array of keys from the map given
+func SortedKeys[K cmp.Ordered, V any](m map[K]V) []K {
+	keys := Keys(m)
+
+	slices.Sort(keys)
+
+	return keys
+}
+
+// Return values of the map given
+func Values[K comparable, V any](m map[K]V) []V {
+	var values []V
+
+	for _, value := range m {
+		values = append(values, value)
+	}
+
+	return values
+}
+
+// Return a sorted array of values from the map given
+func SortedValues[K comparable, V cmp.Ordered](m map[K]V) []V {
+	values := Values(m)
+
+	slices.Sort(values)
+
+	return values
+}
+
 // Return true if the key given is in the map given. Else, false.
 func HasKey[KEY comparable, A any](mapp map[KEY]A, key KEY) bool {
 	for mappKey := range mapp {
@@ -9,4 +54,36 @@ func HasKey[KEY comparable, A any](mapp map[KEY]A, key KEY) bool {
 	}
 
 	return false
+}
+
+// Return true if the maps given are equal, else false. O(nlogn)
+func Equals[K cmp.Ordered, V cmp.Ordered](m1 map[K]V, m2 map[K]V) bool {
+	// Confirm equal lengths
+	if len(m1) != len(m2) {
+		return false
+	}
+
+	// Get sorted values for each map, O(nlogn)
+	keys1 := SortedKeys(m1)
+	keys2 := SortedKeys(m2)
+
+	// Compare keys for equality, O(n)
+	for i := 0; i < len(keys1); i++ {
+		if keys1[i] != keys2[i] {
+			return false
+		}
+	}
+
+	// Get sorted values for each map, O(nlogn)
+	values1 := SortedValues(m1)
+	values2 := SortedValues(m2)
+
+	// Compare values for equality, O(n)
+	for i := 0; i < len(values1); i++ {
+		if values1[i] != values2[i] {
+			return false
+		}
+	}
+
+	return true
 }
--- maps_test.go
+++ maps_test.go
@@ -1,6 +1,7 @@
 package maps
 
 import (
+	"strconv"
 	"testing"
 )
 
@@ -29,5 +30,49 @@ func TestNotHasKey(t *testing.T) {
 
 	if expected != actual {
 		t.Fatalf("\nExpected:\t%t, key was NOT found\nActual:\t\t%t, key was found\n", expected, actual)
+	}
+}
+
+func TestMapEquality(t *testing.T) {
+	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
+	m2 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
+	expected := true
+	actual := Equals(m1, m2)
+
+	if !actual {
+		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are equal\nActual: '" + strconv.FormatBool(actual) + "', they are not equal")
+	}
+}
+
+func TestMapInequalityByLength(t *testing.T) {
+	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
+	m2 := map[string]string{"key1": "value1", "key2": "value2"}
+	expected := false
+	actual := Equals(m1, m2)
+
+	if actual {
+		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
+	}
+}
+
+func TestMapInequalityByKeys(t *testing.T) {
+	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
+	m2 := map[string]string{"key1": "value1", "key2": "value2", "key": "value3"}
+	expected := false
+	actual := Equals(m1, m2)
+
+	if actual {
+		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
+	}
+}
+
+func TestMapInequalityByValues(t *testing.T) {
+	m1 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value3"}
+	m2 := map[string]string{"key1": "value1", "key2": "value2", "key3": "value"}
+	expected := false
+	actual := Equals(m1, m2)
+
+	if actual {
+		t.Fatalf("\nExpected: '" + strconv.FormatBool(expected) + "', they are inequal\nActual: '" + strconv.FormatBool(actual) + "', they are equal")
 	}
 }
