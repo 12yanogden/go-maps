c3aec6e5fe5769c759c640336c7f48f35f4843fb
Ryan Ogden <12yanogden@gmail.com>
2023-12-31 09:34:40 -0500

    add negative test

--- maps_test.go
+++ maps_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestHowKey(t *testing.T) {
+func TestHasKey(t *testing.T) {
 	mapp := map[string]string{
 		"key1": "value1",
 		"key2": "value2",
@@ -13,7 +13,21 @@ func TestHowKey(t *testing.T) {
 	expected := true
 	actual := HasKey[string](mapp, "key2")
 
-	if (expected != actual) {
+	if expected != actual {
 		t.Fatalf("\nExpected:\t%t, key was found\nActual:\t\t%t, key was NOT found\n", expected, actual)
+	}
+}
+
+func TestNotHasKey(t *testing.T) {
+	mapp := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+	expected := false
+	actual := HasKey[string](mapp, "key4")
+
+	if expected != actual {
+		t.Fatalf("\nExpected:\t%t, key was NOT found\nActual:\t\t%t, key was found\n", expected, actual)
 	}
 }
