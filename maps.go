5e812286693adabb702e626367ce0873eef88c83
Ryan Ogden <12yanogden@gmail.com>
2024-09-29 20:43:45 -0400

    add Concat()

--- maps.go
+++ maps.go
@@ -5,55 +5,13 @@ import (
 	"slices"
 )
 
-// Return keys of the map given
-func Keys[K comparable, V any](m map[K]V) []K {
-	var keys []K
-
-	for key := range m {
-		keys = append(keys, key)
+// Concatenate the maps given
+func Concat[K comparable, V any](m1 map[K]V, m2 map[K]V) map[K]V {
+	for key2, value2 := range m2 {
+		m1[key2] = value2
 	}
 
-	return keys
-}
-
-// Return a sorted array of keys from the map given
-func SortedKeys[K cmp.Ordered, V any](m map[K]V) []K {
-	keys := Keys(m)
-
-	slices.Sort(keys)
-
-	return keys
-}
-
-// Return values of the map given
-func Values[K comparable, V any](m map[K]V) []V {
-	var values []V
-
-	for _, value := range m {
-		values = append(values, value)
-	}
-
-	return values
-}
-
-// Return a sorted array of values from the map given
-func SortedValues[K comparable, V cmp.Ordered](m map[K]V) []V {
-	values := Values(m)
-
-	slices.Sort(values)
-
-	return values
-}
-
-// Return true if the key given is in the map given. Else, false.
-func HasKey[KEY comparable, A any](mapp map[KEY]A, key KEY) bool {
-	for mappKey := range mapp {
-		if key == mappKey {
-			return true
-		}
-	}
-
-	return false
+	return m1
 }
 
 // Return true if the maps given are equal, else false. O(nlogn)
@@ -86,4 +44,55 @@ func Equals[K cmp.Ordered, V cmp.Ordered](m1 map[K]V, m2 map[K]V) bool {
 	}
 
 	return true
+}
+
+// Return true if the key given is in the map given. Else, false.
+func HasKey[KEY comparable, A any](mapp map[KEY]A, key KEY) bool {
+	for mappKey := range mapp {
+		if key == mappKey {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Return keys of the map given
+func Keys[K comparable, V any](m map[K]V) []K {
+	var keys []K
+
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	return keys
+}
+
+// Return a sorted array of keys from the map given
+func SortedKeys[K cmp.Ordered, V any](m map[K]V) []K {
+	keys := Keys(m)
+
+	slices.Sort(keys)
+
+	return keys
+}
+
+// Return a sorted array of values from the map given
+func SortedValues[K comparable, V cmp.Ordered](m map[K]V) []V {
+	values := Values(m)
+
+	slices.Sort(values)
+
+	return values
+}
+
+// Return values of the map given
+func Values[K comparable, V any](m map[K]V) []V {
+	var values []V
+
+	for _, value := range m {
+		values = append(values, value)
+	}
+
+	return values
 }
--- maps_test.go
+++ maps_test.go
@@ -5,6 +5,94 @@ import (
 	"testing"
 )
 
+func TestConcat(t *testing.T) {
+	m1 := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+	}
+	m2 := map[string]string{
+		"key4": "value4",
+		"key5": "value5",
+		"key6": "value6",
+	}
+	expected := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+		"key4": "value4",
+		"key5": "value5",
+		"key6": "value6",
+	}
+	actual := Concat(m1, m2)
+
+	if !Equals(actual, expected) {
+		t.Fatalf("\nExpected:\t%v\nActual:\t\t%v\n", expected, actual)
+	}
+}
+
+func TestKeys(t *testing.T) {
+	mapp := map[any]string{
+		1:      "value1",
+		"key2": "value2",
+	}
+	expected := []any{1, "key2"}
+	actual := Keys(mapp)
+	notFound := []any{}
+
+	for actualKey := range actual {
+		found := false
+
+		for expectedKey := range expected {
+			if actualKey == expectedKey {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			notFound = append(notFound, actualKey)
+		}
+	}
+
+	if len(notFound) > 0 {
+		for notFoundKey := range notFound {
+			t.Fatalf("\nExpected:\t%v to be found\nActual:\t\t%v\n", notFoundKey, actual)
+		}
+	}
+}
+
+func TestValues(t *testing.T) {
+	mapp := map[string]any{
+		"key1": 1,
+		"key2": "value2",
+	}
+	expected := []any{1, "value2"}
+	actual := Values(mapp)
+	notFound := []any{}
+
+	for _, actualValue := range actual {
+		found := false
+
+		for _, expectedValue := range expected {
+			if actualValue == expectedValue {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			notFound = append(notFound, actualValue)
+		}
+	}
+
+	if len(notFound) > 0 {
+		for notFoundValue := range notFound {
+			t.Fatalf("\nExpected:\t%v to be found\nActual:\t\t%v\n", notFoundValue, actual)
+		}
+	}
+}
+
 func TestHasKey(t *testing.T) {
 	mapp := map[string]string{
 		"key1": "value1",
